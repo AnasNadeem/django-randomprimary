280e9689b0bace9456b637c475e903e824f51832
Pavan Verma <pavan.verma@gmail.com>
2016-10-22 14:10:56 +0530

    Added app version

---
+++ django_randomprimary/__init__.py
@@ -0,0 +1,5 @@
+__author__ = 'Pavan Verma @ P3 Infotech Solutions Pvt. Ltd. [@YingYangPavan]'
+__description__ = "A Django application to generate random model ids"
+__version__ = '1.0.1'
+
+default_app_config = 'django_randomprimary.apps.AppConfig'
